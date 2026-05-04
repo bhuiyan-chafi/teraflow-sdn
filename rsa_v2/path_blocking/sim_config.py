19204c085bb6878053b88d9c6a88c48599390683
bhuiyan-chafi <a.bhuiyan@studenti.unipi.it>
2026-05-04 16:41:05 +0200

    removed additional files

--- rsa_v2/path_blocking/sim_config.py
+++ rsa_v2/path_blocking/sim_config.py
@@ -3,21 +3,21 @@ import os
 # Stopping criteria: CI <= 5% or 10% of blocking probability at 95% confidence level
 
 # NSF 14-node topology (legacy):
-# NODES = ["RDMwa", "RDMca1", "RDMca2", "RDMut", "RDMco", "RDMne", "RDMtx", 
-#          "RDMil", "RDMmi", "RDMpa", "RDMny", "RDMnj", "RDMma", "RDMga"]
+NODES = ["RDMwa", "RDMca1", "RDMca2", "RDMut", "RDMco", "RDMne", "RDMtx", 
+         "RDMil", "RDMmi", "RDMpa", "RDMny", "RDMnj", "RDMma", "RDMga"]
 
 # Pan-EU 28-node topology:
-NODES = [
-    "RDMdublin", "RDMglasgow", "RDMlondon", "RDMoslo", "RDMstockholm",
-    "RDMcopenhagen", "RDMhamburg", "RDMamsterdam", "RDMbrussels", "RDMparis",
-    "RDMbordeaux", "RDMlyon", "RDMstrasbourg", "RDMfrankfurt", "RDMberlin",
-    "RDMwarsaw", "RDMprague", "RDMmunich", "RDMzurich", "RDMvienna",
-    "RDMbudapest", "RDMmilan", "RDMturin", "RDMrome", "RDMbarcelona",
-    "RDMmadrid", "RDMathens", "RDMbelgrade",
-]
+# NODES = [
+#     "RDMdublin", "RDMglasgow", "RDMlondon", "RDMoslo", "RDMstockholm",
+#     "RDMcopenhagen", "RDMhamburg", "RDMamsterdam", "RDMbrussels", "RDMparis",
+#     "RDMbordeaux", "RDMlyon", "RDMstrasbourg", "RDMfrankfurt", "RDMberlin",
+#     "RDMwarsaw", "RDMprague", "RDMmunich", "RDMzurich", "RDMvienna",
+#     "RDMbudapest", "RDMmilan", "RDMturin", "RDMrome", "RDMbarcelona",
+#     "RDMmadrid", "RDMathens", "RDMbelgrade",
+# ]
 BIT_RATES = [100, 200, 400]
 BIT_RATE_PROBS = [0.5, 0.25, 0.25]
-ERLANGS = [250, 350, 450, 550, 650, 750, 850, 950, 1050, 1150 ]
+ERLANGS = [500]
 
 # CI-based stopping parameters
 MAX_REQUESTS = 1000000  # Maximum number of independent trials (10^4)
